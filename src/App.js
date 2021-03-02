552ce538220ed3f541a61fa8a0e18da3d65fbb38
Reece Jones <reece.c.jones@gmail.com>
2021-03-03 02:44:06 +1100

    working sond router

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ import {
   useParams,
   useRouteMatch
 } from "react-router-dom";
-
-import React, { useState } from 'react';
+import parse from 'html-react-parser'
+// import React, { useState } from 'react';
 
 import { songs } from './data';
 
 import Home from "./Views/Home.js";
-import Login from "./Views/Login.js";
-import Dashboard from "./Views/Dashboard.js";
-import Song from "./Views/Song.js";
+// import Login from "./Views/Login.js";
+// import Dashboard from "./Views/Dashboard.js";
+// import Song from "./Views/Song.js";
 
 
 function App() {
@@ -26,6 +26,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <div className="social-sharing">
+        <a href="https://www.facebook.com/sharer/sharer.php?u=https://grubbyurchins-songs.web.app/ Shanties - The best way to sing together  " target="_blank" className="message__share__icon">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg>
+        </a>
+        <a href="https://twitter.com/home?status=https://grubbyurchins-songs.web.app/ Shanties - The best way to sing together " target="_blank" className="message__share__icon">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg>
+        </a>
+      </div>
         <Router>
           <div>
             <ul>
@@ -59,58 +67,53 @@ function Songs() {
   // relative to the parent route, while the `url` lets
   // us build relative links.
   let { path, url } = useRouteMatch();
-  const testSongs = [
-      {name:'Kickass',
-        lyrics:"Blue Murder sing Bully in the Alley Take in yer lines and make her fast\ boys/ Drop in the pawls, we've heaved enough now",
-        artist:'Reece Jones'},
-      {name:'kity kity',lyrics:'blah blah blahblah blah blahblah blah blahblah blah blahblah blah blah',artist:'Reece Jones'},
-      {name:'boom boom',lyrics:'blah blah blahblah blah blah',artist:'Reece Jones'},
-      {name:'power!',lyrics:'mwhahahshs blah blah',artist:'Reece Jones'},
-      {name:'lets do this!',lyrics:'killer killer chicken dinner blah blah',artist:'Reece Jones'},
-    ]
-
-      // simply do so by passing it through the component prop.
-
 
   return (
     <div>
-      <h2>Songs</h2>
+      <h2>Shanties</h2>
       <ul>
-        { testSongs.map(song => {
+        { songs.map(song => {
           return(
-            <li key={song.name}>
+            <li key={song.id}>
               <Link to={`${url}/${song.name}`} >{song.name}</Link>
             </li>
           )})
         }
-
-        <li>
-          <Link to={`${url}/props-v-state`}>Props v. State</Link>
-        </li>
       </ul>
 
       <Switch>
         <Route exact path={path}>
-          <h3>Please select a Song.</h3>
+          <h3>Please select a Shanty.</h3>
         </Route>
         <Route path={`${path}/:songId`}>
-          <Topic />
+          <Topic testSongs={songs} />
         </Route>
       </Switch>
     </div>
   );
 }
 
-function Topic() {
+function Topic(props) {
   // The <Route> that rendered this component has a
   // path of `/Songs/:topicId`. The `:topicId` portion
   // of the URL indicates a placeholder that we can
   // get from `useParams()`.
   let { songId } = useParams();
 
+  console.log('props + id ', props, songId)
+  const song = props.testSongs.filter(function (song) {
+    console.log('each song',song)
+    return song.name === songId;
+  })[0];
+  console.log(song)
+
   return (
     <div>
-      <h3>{songId}</h3>
+      <h3>{song.name}</h3>
+      <div>
+        {parse(song.lyrics)}
+      </div>
+      <h4>{song.version}</h4>
     </div>
   );
 }
--- src/data.js
+++ src/data.js
@@ -1,34 +1,35 @@
 // data, array of objects
-let songs=[{
-    name:'ruby',
-    id:1,
-    description:'introduction to ruby and ruby objects',
-    instructors:[
-    {name:'Paul',id:1,favorite_game:'metal gear',url:'https://images.app.goo.gl/y5tLE61vg3vABcmq7'},
-    {name:'Sheena',id:2,favorite_game:'minecraft',url:'https://images.app.goo.gl/meVgyfWmStzdBhKQ9'},
-    {name:'Mark',id:3,favorite_game:'grand theft auto',url:'https://images.app.goo.gl/B8Gpt9BjiuGyb3ey6'}
-    ]
-},{
-    name:'ruby on rails',
-    id:2,
-    description:'introduction to active records and ruby on rails framework',
-    instructors:[
-    {name:'Raul',id:1,favorite_game:'fornite',url:'https://images.app.goo.gl/t7LjYctCSzF9nSDc8'},
-    {name:'Sheena',id:2,favorite_game:'minecraft',url:'https://images.app.goo.gl/meVgyfWmStzdBhKQ9'},
-    {name:'Ann',id:3,favorite_game:'overwatch',url:'https://images.app.goo.gl/Wyh2fmRkqAu5WLqNA'},
-    {name:'Mark',id:4,favorite_game:'grand theft auto',url:'https://images.app.goo.gl/B8Gpt9BjiuGyb3ey6'}
-    ]
-},{
-    name:'javascripts',
-    id:3,
-    description:'introduction to javascripts and DOM manipulation',
-    instructors:[
-    {name:'Raul',id:1,favorite_game:'fornite',url:'https://images.app.goo.gl/t7LjYctCSzF9nSDc8'},
-    {name:'Sheena',id:2,favorite_game:'minecraft',url:'https://images.app.goo.gl/meVgyfWmStzdBhKQ9'},
-    {name:'Epic',id:3,favorite_game:'call of duty',url:'https://images.app.goo.gl/eXoVyBjz9NCGgott8'},
-    {name:'Mark',id:4,favorite_game:'grand theft auto',url:'https://images.app.goo.gl/B8Gpt9BjiuGyb3ey6'}
-    ]
-}]
+
+const songs = [
+    {
+        id: 1,
+        name:'Alabama',
+        lyrics:'<div class="lyrics"><p class="verse">When the Alabama"s keel was laid <br><span class="chorus">Roll Alabama, roll</span><br>They laid her keel at Birkenhead <br><span class="chorus">Oh, roll Alabama, roll</span></p><p class="verse">Oh, she was built in Birkenhead <br>Built in the yard of Jonathan Laird</p><p class="verse">Away down the Mersey she rolled one day <br>And across the "Western" she ploughed her way</p><p class="verse">With British guns, oh, she was stocked <br>She sailed from Fayal, in Cherbourg she docked</p><p class="verse">To fight the north Semmes did employ <br>Any method to kill and destroy</p><p class="verse">But off Cherbourg the Kearsage lay tight <br>Awaiting was Winslow to start a good fight</p><p class="verse">Outside the three mile limit they fought <br>An Semmes escaped on a fine British yacht</p><p class="verse">The Kearsage won - Alabama so brave <br>Sank to the bottom of a watery grave</p></div>',
+        version:'Colcord'
+    },
+    {
+        id: 2,
+        name:'The Black Ball Line',
+        lyrics:'<div class="lyrics"><p class="verse">In the Black Ball line I served my time<br><span class="chorus">To me way-aye-aye, hurray-ah</span><br>And thats the line where you can shine<br><span class="chorus">Hurrah for the Black Ball Line</span><br></p><p class="verse">The Black Ball Ships are good and true<br>They are the ships for me and you<br></p><p class="verse">For once there was a Black Ball Ship<br>That fourteen knots an hour could clip<br></p><p class="verse">Theyll carry you along through frost and snow<br>And take you where the wind dont blow<br></p><p class="verse">You will surely find a rich gold mine<br>Just take a trip in the Black Ball Line<br></p><p class="verse">Just take a trip to Liverpool<br>To Liverpool, that Yankee school<br></p><p class="verse">The Yankee sailors youll see there<br>With red-top boots and short-cut hair<br></p><p class="verse">At Liverpool docks we bid adieu<br>To Poll and Bet and lovely Sue<br></p><p class="verse">And now were bound for New York Town<br>Its there well drink, and sorrow drown<br></p></div>',
+        version:'Andrew Draskoy version'
+    },
+    {
+        id: 3,
+        name:'Whiskey-O',
+        lyrics:'<div class="lyrics"><p class="verse"><span class="chorus">Whiskey-o, Johny-o</span><br><span class="chorus">Rise her up from down below</span><br><span class="chorus">Whiskey, whiskey, whiskey-o</span><br><span class="chorus">Up aloft this yard must go</span><br><span class="chorus">John rise her up from down below</span><br></p><p class="verse">Now whiskey is the life of man<br>Always was since the world began<br></p><p class="verse">Now whiskey gave me a broken nose<br>And whiskey made me pawn me clothes<br></p><p class="verse">Now whiskey is the life of man<br>Whiskey from that old tin can<br></p><p class="verse">I thought I heard the first mate say<br>I treats me crew in a decent way<br></p><p class="verse">A glass of whiskey all around<br>And a bottle full for the shanty man<br></p><p class="verse">(Spoken: Up she blew!)<br></p></div>',
+        version:'Donkeys Breakfast'},
+    {
+        id: 4,
+        name:'Whiskey Johnny',
+        lyrics:'<div class="lyrics"><p class="verse">Whiskey is the life of man<br>Always was since the world began<br></p><p class="verse"><span class="chorus">Whiskey-o, Johnny-o</span><br><span class="chorus">John rise her up from down below</span><br><span class="chorus">Whiskey, whiskey, whiskey-o</span><br><span class="chorus">Up aloft this yard must go</span><br><span class="chorus">John rise her up from down below</span><br></p><p class="verse">Whiskey here, whiskey there<br>Whiskey almost everywhere<br></p><p class="verse">Whiskey up and whiskey down<br>Whiskey all around the town<br></p><p class="verse">Whiskey killed me poor old dad<br>Whiskey drove me mother mad<br></p><p class="verse">My wife and I do not agree<br>She puts whiskey in her tea<br></p><p class="verse">I had a girl and her name was Lize<br>She puts whiskey in her pies<br></p><p class="verse">Oh whiskey straight, and whiskey strong<br>Give me some whiskey and Ill sing you a song<br></p><p class="verse">If whiskey comes too near my nose<br>I tip it up and down she goes<br></p><p class="verse">Some likes whiskey, some likes beer<br>I wisht I had a barrel here<br></p><p class="verse">Whiskey made me pawn me clothes<br>Whiskey gave me this broken nose<br></p><p class="verse">Oh the mate likes whiskey, the skipper likes rum<br>The sailors like both but me cant get none<br></p><p class="verse">Whiskey is the life of man<br>Whiskey from that old tin can<br></p><p class="verse">I thought I heard the first mate say<br>I treats me crew in a decent way<br></p><p class="verse">If whiskey was a river and I could swim<br>Id say here goes and dive right in<br></p><p class="verse">If whiskey was a river and I was a duck<br>Id dive to the bottom and never come up<br></p><p class="verse">I wisht I knew where whiskey grew<br>Id eat the leaves and the branches too<br></p><p class="verse">A tot of whiskey all around<br>And a bottle full for the shanty man<br></p></div>',
+        version:'Andrew Draskoy version'},
+    {
+        id: 5,
+        name:'Bully in the Alley',
+        lyrics:'<div class="lyrics"><p class="verse">So help me Bob, Im bully in the alley<br><span class="chorus">Way, hey, bully in the alley</span><br>So help me Bob, Im bully in the alley<br><span class="chorus">Bully down in Shinbone Al</span><br></p><p class="verse">Oh, Sally is the girl down in our alley<br>Now, Sally is the gal that I spliced nearly<br></p><p class="verse">So help me Bob, Im bully in the alley<br>So help me Bob, Im bully in the alley<br></p><p class="verse">Ill leave my Sal to go a-sailin<br>Ill leave my gal and go a-whalin<br></p><p class="verse">So help me Bob, Im bully in the alley<br>So help me Bob, Im bully in the alley<br></p></div>',
+        version:'Nola Johnston'
+    },
+]
 
 module.exports = {
     songs
